b13099832ffbfe510edd69e340643a9bd469e318
Nikita <silvernikchuprov@gmail.com>
2022-11-17 17:04:42 +0300

    task 63 done

---
+++ Task63/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите целое число N");
+int numN = Convert.ToInt32(Console.ReadLine());
+
+void NaturalNumbers (int num)
+{
+    if(num == 0) return;
+    Console.Write($"{num} ");
+    NaturalNumbers(num - 1);
+}  
+
+NaturalNumbers(numN);
